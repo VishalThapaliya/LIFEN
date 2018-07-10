d7bbc31c29f423e9e83a940058d95e1f0ad248c5
Bishal Thapaliya <vishal.thapaliya@gmail.com>
2018-07-10 17:08:27 +0200

    Add files via upload

---
+++ lvl4/src/components/totalBinary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TotalBinary extends React.Component{
+  render(){
+    return(
+        <h2>
+
+          Total Binary(ies) : {this.props.count}
+        </h2>
+      )
+    }
+}
+
+export default TotalBinary;
